f7570b9f598e8af81037361ec31b73d009e9ad07
Thomas Gleinxer <tglx@linutronix.de>
2011-10-14 12:44:41 +0100

    ARM: 7133/1: SMP: fix per cpu timer setup before the cpu is marked online
    The problem is related to the early enabling of interrupts and the
    per cpu timer setup before the cpu is marked online. This doesn't
    need to be done in order to call calibrate_delay().
    
    calibrate_delay() monitors jiffies, which are updated from the CPU
    which is waiting for the new CPU to set the online bit.
    
    So simply calibrate_delay() can be called on the new CPU just from
    the interrupt disabled region and move the local timer setup after
    stored the cpu data and before enabling interrupts.
    
    This solves both the cpu_online vs. cpu_active problem and the
    affinity setting of the per cpu timers.
    
    Change-Id: Iadb01889e859eedca426af7dbff2a015bc145e82
    Signed-off-by: Thomas Gleinxer <tglx@linutronix.de>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -325,9 +325,6 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	 */
 	platform_secondary_init(cpu);
 
-	/*
-	 * Enable local interrupts.
-	 */
 	notify_cpu_starting(cpu);
 	local_irq_enable();
 	local_fiq_enable();
@@ -351,6 +348,13 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 		cpu_relax();
 
 	printk(KERN_INFO"CPU1 is up\n");
+
+	/*
+	 * cpu_active bit is set, so it's safe to enalbe interrupts
+	 * now.
+	 */
+	local_irq_enable();
+	local_fiq_enable();
 
 	/*
 	 * OK, it's off to the idle thread for us
