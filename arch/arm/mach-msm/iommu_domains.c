094a42b5bf00e43e73c41cc1260c8b3e5ff17207
Laura Abbott <lauraa@codeaurora.org>
2012-06-07 17:51:53 -0700

    msm: Select page protection for IOMMU
    IOMMU mapping requires explicit attributes for mapping instead
    of relying on defaults. Add explicit page protection attributes
    when mapping contiguous buffers.
    
    Change-Id: I01e19eebd5b1a1c2456ba99861641baafbabe97a
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/mach-msm/iommu_domains.c
+++ arch/arm/mach-msm/iommu_domains.c
@@ -123,6 +123,8 @@ static int msm_iommu_map_iova_phys(struct iommu_domain *domain,
 {
 	int ret;
 	struct scatterlist *sglist;
+	int prot = IOMMU_WRITE | IOMMU_READ;
+	prot |= cached ? IOMMU_CACHE : 0;
 
 	sglist = vmalloc(sizeof(*sglist));
 	if (!sglist) {
@@ -135,7 +137,7 @@ static int msm_iommu_map_iova_phys(struct iommu_domain *domain,
 	sglist->offset = 0;
 	sglist->dma_address = phys;
 
-	ret = iommu_map_range(domain, iova, sglist, size, cached);
+	ret = iommu_map_range(domain, iova, sglist, size, prot);
 	if (ret) {
 		pr_err("%s: could not map extra %lx in domain %p\n",
 			__func__, iova, domain);
